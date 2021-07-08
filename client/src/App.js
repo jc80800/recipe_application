d8387ef3c9367102fa4c2391264d68a131a1c16e
Hai-Yen's RIT <hnn2306@rit.edu>
2021-07-08 14:03:39 -0400

    starting react stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component{
+  
+    render(){
+        return(
+            <div classNam= 'App'>
+                <h1> Recipe Domain </h1>
+                <h1> Team Gyrados</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
