21e2f72fd6c5e08a871788ed0343cf0320b5990d
jc8080 <jc8080@rit.edu>
2021-07-08 02:29:44 -0400

    Implemented minimal API server using the Postgres database. Added minimal code for express routing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,45 @@
 const express = require("express");
 const app = express();
+const recipe_model = require('./query');
+
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+  });
+
+
+app.get('/', (req, res) => {
+    recipe_model.getRecipes()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+app.post('/recipes', (req, res) => {
+    recipe_model.createRecipes(req.body)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+        })
+    })
+
+app.delete('/recipes/:id', (req, res) => {
+    recipe_model.deleteRecipes(req.params.id)
+    .then(response => {
+        res.status(200).send(response);
+    })
+    .catch(error => {
+        res.status(500).send(error);
+        })
+    })
 
 app.listen(5000, () => {
     console.log("Server listening to 5000");
--- server/query.js
+++ server/query.js
@@ -8,7 +8,41 @@ const pool = new Pool({
   port: 5432,
 })
 
-// //pool.query("...", (err, res) => {
-//     console.log(err, res);
-//     pool.end();    
-// })
+const getRecipes = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+const createRecipes = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+    pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new merchant has been added added: ${results.rows[0]}`)
+    })
+  })
+}
+const deleteRecipes = () => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Merchant deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getRecipes,
+  createRecipes,
+  deleteRecipes,
+}
