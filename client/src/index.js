71e9e6c96eda010025a58c9a24859023807b1236
Hai-Yen's RIT <hnn2306@rit.edu>
2021-07-10 17:54:41 -0400

    Sign up + application page

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Login from './Login';
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-  document.getElementById('root')
-
-  // <Route path="/" component={ Login }>
-  //   <IndexRoute component={ App } />
-  //   {/* <Route path="detail/:repo" component={ Detail } /> */}
-  // </Route>
- 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
